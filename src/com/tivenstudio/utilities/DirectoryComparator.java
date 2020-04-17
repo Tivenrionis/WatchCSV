cb22fda7adde3a7f3a59df7abb1a57b05e9934de
Tiven <tivenstudio@gmail.com>
2020-04-17 12:37:22 +0200

    DirectoryComparator: write Javadoc comments

--- src/com/tivenstudio/utilities/DirectoryComparator.java
+++ src/com/tivenstudio/utilities/DirectoryComparator.java
@@ -4,12 +4,26 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * @author Michał Wesołowski
+ * @version 1.0 <br>
+ * Date: 09.04.2020 <br>
+ * Time: 16:50 <br>
+ * Class name: DirectoryComparator<br>
+ * Description: This class is used to compare two different File objects
+ * referring to two different directory paths.
+ */
 public class DirectoryComparator {
 
-
-    public DirectoryComparator() {
-    }
-
+    /**
+     * This method takes two File objects, each denotes to directory path
+     * and returns files not present in second directory.
+     * It takes first given directory and checks if the files located there
+     * are present in second given directory path.
+     * @param dirA - Directory to check if its files are present in <b>dirB</b>
+     * @param dirB - Directory to check if its containing files of directory <b>dirA</b>
+     * @return Set of File objects representing result of subtracting <b>dirB</b> from <b>dirA</b>.
+     */
     public static Set<File> compare(File dirA, File dirB) {
         Set<File> toBeDeleted = new HashSet<>();
         Set<File> filesInDirA = new DirectoryReader(dirA).getFiles();
